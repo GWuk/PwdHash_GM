703b5ebedec5e22ee1a4489ee790b7cb015a8ac8
GWu <GWuk@users.noreply.github.com>
2017-09-10 11:24:37 +0200

    repo URLs

--- PwdHash.user.js
+++ PwdHash.user.js
@@ -2,12 +2,12 @@
 // @name          PwdHash
 // @namespace     PwdHash_GM
 // @description   Automatically generates per-site passwords if you prefix your password with @@ or F2. Based on Stanford PwdHash (https://pwdhash.github.io/website/), migrated to GreaseMonkey and PBKDF2-SHA256 support, based on https://www.cl.cam.ac.uk/%7Edl551/pwdhash/.
-// @require       https://gitcdn.link/repo/GWuk/MonkeyConfig/master/monkeyconfig.js
-// @require       https://gitcdn.link/repo/GWuk/pwdhash-firefox/master/chrome/content/md5.js
-// @require       https://gitcdn.link/repo/GWuk/forge-dist/master/dist/forge.min.js
-// @require       https://gitcdn.link/repo/GWuk/pwdhash-firefox/master/chrome/content/domain-extractor.js
-// @require       https://gitcdn.link/repo/GWuk/pwdhash-firefox/master/chrome/content/hashed-password.js
-// @require       https://gist.githubusercontent.com/GWuk/70ec44fb092534c208533fe51fd3c0c0/raw/44fff2cb77eaff3e4937f372a4a4f7a8fd92c128/stanford-pwdhash.js
+// @require       https://raw.githubusercontent.com/GWuk/PwdHash_GM/master/monkeyconfig.js
+// @require       https://raw.githubusercontent.com/GWuk/PwdHash_GM/master/md5.js
+// @require       https://raw.githubusercontent.com/GWuk/PwdHash_GM/master/forge.min.js
+// @require       https://raw.githubusercontent.com/GWuk/PwdHash_GM/master/domain-extractor.js
+// @require       https://raw.githubusercontent.com/GWuk/PwdHash_GM/master/hashed-password.js
+// @require       https://raw.githubusercontent.com/GWuk/PwdHash_GM/master/stanford-pwdhash.js
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @grant         GM_addStyle
